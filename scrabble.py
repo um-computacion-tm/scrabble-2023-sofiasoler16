23e7cbe94fd9f5983dd8198cf340d0348fdfb6d0
sofia16044 <s.soler@alumno.um.edu.ar>
2023-08-21 20:09:19 -0300

    Scrabble

--- scrabble.py
+++ scrabble.py
@@ -1,5 +1,7 @@
 import random
 
+class YaHaySuficientes(Exception):
+    pass
 
 class Tile:
     def __init__(self, letter, value, cant):
@@ -16,18 +18,35 @@ class BagTiles:
                     "U":Tile("U",1,5), "V":Tile("V",4,1), "X":Tile("X",8,1), "Y":Tile("Y",4,1), "Z":Tile("Z",10,1),
                     "LL":Tile("LL",8,1), "RR":Tile("RR",8,1)
                     }
+        self.actualizado = {"A":Tile("A",1,12), "B":Tile("B",3,2), "C":Tile("C",3,4), "D":Tile("D",2,5), "E":Tile("E",1,12),
+                    "F": Tile("F",4,1), "G":Tile("G",2,2), "H":Tile("H",4,2), "I":Tile("I",1,6), "J":Tile("J",8,1),
+                    "L":Tile("L",1,4), "M":Tile("M",3,2), "N":Tile("N",1,5), "Ñ":Tile("Ñ",8,1), "O":Tile("O",1,9),
+                    "P":Tile("P",3,2), "Q":Tile("Q",5,1), "R":Tile("R",1,5), "S":Tile("S",1,6), "T":Tile("T",1,4),
+                    "U":Tile("U",1,5), "V":Tile("V",4,1), "X":Tile("X",8,1), "Y":Tile("Y",4,1), "Z":Tile("Z",10,1),
+                    "LL":Tile("LL",8,1), "RR":Tile("RR",8,1)
+                    }
     def take(self):
         letras = list(self.tiles.keys())
         elec = random.choice(letras)
         print(elec)
         print(self.tiles[elec].cant)
-        (self.tiles[elec].cant) -= 1
-        print (self.tiles[elec].cant)
+        (self.actualizado[elec].cant) -= 1
+        print(self.actualizado[elec].cant)
 
-#Como accedo a ellos
+    def put(self, letra):
+        if self.actualizado[letra].cant < self.tiles[letra].cant:
+            (self.actualizado[letra].cant) += 1
+            print(self.actualizado[letra].cant)
+        else:
+            raise YaHaySuficientes()          
+
+
 bag = BagTiles()
-letra = "C"
-print(bag.tiles[letra].cant)
+
+
+
 #20 ago. Agregamos valor y cant a cada letra. Vimos como acceder a ellos. 
 #Metodo para restar cantidad cuando se saca una letra
+
+#21 ago. Hacer funcion para agregar letras. Agregamos diccionario de actualizados
 
