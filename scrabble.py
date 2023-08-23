474cea0c1c55f807ec4160e8ab587a2710099bf6
sofia16044 <s.soler@alumno.um.edu.ar>
2023-08-23 10:03:30 -0300

    Scrabble game

--- scrabble.py
+++ scrabble.py
@@ -32,18 +32,63 @@ class BagTiles:
         print(self.tiles[elec].cant)
         (self.actualizado[elec].cant) -= 1
         print(self.actualizado[elec].cant)
+        return(elec)
 
     def put(self, letra):
         if self.actualizado[letra].cant < self.tiles[letra].cant:
             (self.actualizado[letra].cant) += 1
             print(self.actualizado[letra].cant)
         else:
-            raise YaHaySuficientes()          
-
+            raise YaHaySuficientes()
 
 bag = BagTiles()
+print(bag.take())
+
+class Player():
+    def __init__ (self):
+        self.tiles = []
+    def repartidas(self):
+        self.tiles.append(bag.take())
 
 
+player = Player()
+
+
+class Board:
+    def __init__(self):
+        self.grid = [
+            [ Cell(1, '') for _ in range(15) ]
+            for _ in range(15)
+        ]
+
+        
+#Celda en especifico, como agregarle un multiplicador a 1 celda en especifico
+class Cell:
+    def __init__(self, multiplayer, multiplayer_type):
+        self.multiplayer = multiplayer
+        self.multiplayer_type = multiplayer_type
+        self.letter = None
+
+    def add_letter(self, letter:Tile):
+        self.letter = letter
+
+    def calculate_value(self):
+        if self.letter is None:
+            return 0
+        if self.multiplayer_type == 'letter':
+            return self.letter.value * self.multiplayer
+        else:
+            return self.letter.value
+    
+
+board = Board()
+
+class ScrabbleGame():
+    def __init__(self, players_count):
+        self.board = Board()
+        self.players = []
+        for _ in range(players_count):
+            self.players.append(Player())
 
 #20 ago. Agregamos valor y cant a cada letra. Vimos como acceder a ellos. 
 #Metodo para restar cantidad cuando se saca una letra
