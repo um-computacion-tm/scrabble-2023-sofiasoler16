2db57c5b1e6e64761a724fd1717a528691ec639e
sofia16044 <s.soler@alumno.um.edu.ar>
2023-09-01 19:51:27 -0300

    test

--- tests/test_scrabble.py
+++ tests/test_scrabble.py
@@ -12,16 +12,6 @@ class TestTiles(unittest.TestCase):
         self.assertEqual(tile.letter, 'A')
         self.assertEqual(tile.cant, 12)
         self.assertEqual(tile.value, 1)
-    def test_tile_valueycantB(self):
-        tile = Tile('B',3, 2)
-        self.assertEqual(tile.letter, 'B')
-        self.assertEqual(tile.cant, 2)
-        self.assertEqual(tile.value, 3)
-    def test_tile_valueycantH(self):
-        tile = Tile('H',4, 2)
-        self.assertEqual(tile.letter, 'H')
-        self.assertEqual(tile.cant, 2)
-        self.assertEqual(tile.value, 4)
 
 class TestBag(unittest.TestCase):
     @patch("random.choice",return_value = "A")
